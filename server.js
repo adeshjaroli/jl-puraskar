dc2d3ba72105528c59b28988fe2e24c41dc901b5
adeshjaroli <jlfixings@gmail.com>
2024-12-29 18:03:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const adminRouter = require("./adminserver");
 const userRouter = require('./userserver');
 const dealerRouter = require("./dealerserver"); // Import the new dealer server
 const path = require('path');
-const moment = require('moment-timezone');  // Import moment-timezone
 
 
 const app = express();
@@ -27,11 +26,11 @@ app.use("/dealer", dealerRouter); // Dealer routes
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Set the default timezone globally for the app
-moment.tz.setDefault('Asia/Kolkata'); // Set India Standard Time (IST) globally
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
 
